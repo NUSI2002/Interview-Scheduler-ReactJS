f21c8e8a0404d876629812b52335b6db5644900b
Nusaiba Fazal <nusaiba2904@gmail.com>
2024-07-28 12:24:06 +0530

    Updates

---
+++ precache-manifest.1224ab04a1715645f4580a31822a2803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e1a624b86b53a471a41fec02d4da80",
+    "url": "/Interview-Scheduler-ReactJS/index.html"
+  },
+  {
+    "revision": "742d12274882602a3a94",
+    "url": "/Interview-Scheduler-ReactJS/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "577987d92b9db0006dc4",
+    "url": "/Interview-Scheduler-ReactJS/static/js/2.b4a1ce99.chunk.js"
+  },
+  {
+    "revision": "742d12274882602a3a94",
+    "url": "/Interview-Scheduler-ReactJS/static/js/main.bd7e222c.chunk.js"
+  },
+  {
+    "revision": "e1159b1d387973a96ac7",
+    "url": "/Interview-Scheduler-ReactJS/static/js/runtime~main.2b30964d.js"
+  }
+]);
